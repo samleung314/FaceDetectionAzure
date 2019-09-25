e350b69170ddf8ca1c13510b4321374c87e1c089
Sam Leung <37159333+samleung314@users.noreply.github.com>
2019-09-24 19:32:57 -0500

    Add project files.

---
+++ FaceDetectionAzure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FaceDetectionAzure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
